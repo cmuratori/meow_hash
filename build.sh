33f8cfd27c538c7cb93bfc7558f5667d52d5d825
Martins Mozeiko <martins.mozeiko@gmail.com>
2018-11-02 23:33:41 -0700

    Allow to override compiler in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
+
+set -eu
+
+CXX=${CXX:-clang++}
+
 mkdir -p build
-clang++ $* -I./ meow_example.cpp -O3 -mavx -maes -obuild/meow_example
-clang++ $* -I./ utils/meow_test.cpp -O3 -mavx -maes -obuild/meow_test
-clang++ $* -I./ utils/meow_search.cpp -O3 -mavx -maes -obuild/meow_search
-clang++ $* -I./ utils/meow_bench.cpp -O3 -mavx -maes -obuild/meow_bench
+${CXX} $* -I. meow_example.cpp -O3 -mavx -maes -o build/meow_example
+${CXX} $* -I. utils/meow_test.cpp -O3 -mavx -maes -o build/meow_test
+${CXX} $* -I. utils/meow_search.cpp -O3 -mavx -maes -o build/meow_search
+${CXX} $* -I. utils/meow_bench.cpp -O3 -mavx -maes -o build/meow_bench
